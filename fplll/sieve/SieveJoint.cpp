83ba5c45fd2dc6ec6e361fc8213a9bb53a1e7f73
Elena <elena@dhcp-13-102.lip.ens-lyon.fr>
2017-02-14 16:07:57 +0100

    conv_sample_to_latticepoint

--- fplll/sieve/SieveJoint.cpp
+++ fplll/sieve/SieveJoint.cpp
@@ -31,18 +31,20 @@
 //actual code starts here.
 //Be aware that this code may be read twice.
 
+template<class ET>
+void SieveIteration (LatticePoint<ET> *p);
 
 template<class ET>
 void Sieve<ET,GAUSS_SIEVE_IS_MULTI_THREADED>::run_2_sieve()
 {
     using SieveT = Sieve<ET,GAUSS_SIEVE_IS_MULTI_THREADED>;
 
-    //want to put my basis-vectors into the main_list
+    //want to put my basis-vectors into the main_list --Make a separate function for that
 
     unsigned int n = lattice_rank;
     auto it = main_list.before_begin();
     for (unsigned int i=0; i<n; i++) {
-        LatticePoint<ET> p (  conv_to_lattice_point (original_basis[i]) );
+        LatticePoint<ET> p (  conv_matrixrow_to_lattice_point (original_basis[i]) );
         it = main_list.emplace_after(it, p);
         //it = main_list.insert_after(it, p);
     }
@@ -52,10 +54,38 @@ void Sieve<ET,GAUSS_SIEVE_IS_MULTI_THREADED>::run_2_sieve()
     
      */
     
+    int i=0;
+    int MaxIteration = 10;
+    
+    LatticePoint<ET> const *p;
+    NumVect<ET> sample;
+    
+    while(i < MaxIteration) // TerminationCondition Here
+    {
+        if (main_queue.empty())
+        {
+            sample = sampler -> sample();
+            p = conv_sample_to_lattice_point(sample);
+        }
+        else
+        {
+            p =  &(main_queue.top()); //why does this return contst?
+            main_queue.pop();
+        }
+        
+        //SieveIteration(p);
+        
+        i++;
+    }
     
     
-    
-};
+}
+
+//template<class ET> - does not work (read twice)
+//void SieveIteration (LatticePoint<ET> p)
+//{
+//    cout << "running SieveIteration " << endl;
+//};
 
 #define SIEVE_JOINT_CPP
 #endif
