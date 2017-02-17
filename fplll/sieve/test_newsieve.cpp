dc62e67d90e8f1142994c82334fcbb7ff6ed4be4
Elena <elena@dhcp-13-102.lip.ens-lyon.fr>
2017-02-17 14:42:38 +0100

    files for test. changed the make file

---
+++ fplll/sieve/test_newsieve.cpp
@@ -0,0 +1,24 @@
+
+#include "sieve_main.h"
+#include "fplll.h"
+//#include "LatticePoint.h"
+//#include "PointList.h"
+#include <thread>
+#include <chrono>
+#include "SieveGauss.h"
+
+using namespace fplll;
+
+
+
+int main(int argc, char **argv)
+{
+
+    cout << "Hello " << endl;
+    //cout << "B[1] = " << B[1] << endl;
+    //auto start = std::chrono::high_resolution_clock::now();
+//    auto finish = std::chrono::high_resolution_clock::now();
+//    auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(finish-start);
+//    cout << " Time taken: " << microseconds.count()/1000000.0 << "sec" << endl;
+  return 1;
+}
