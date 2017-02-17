f0579654e2d35a19ee91c30f97f1dbffd5c81383
Elena <elena@dhcp-13-102.lip.ens-lyon.fr>
2017-02-17 15:20:25 +0100

    test_newsieve

--- fplll/sieve/test_newsieve.cpp
+++ fplll/sieve/test_newsieve.cpp
@@ -9,13 +9,38 @@
 
 using namespace fplll;
 
+// Z_NR can be either long, double, or mpz_t
 
+template <class ZT>
+void test_run_sieve(int dim)
+{
+    TerminationConditions< Z_NR<ZT> > term_cond; //sets target-legnth as Mink. bound
+    ZZ_mat<ZT> B;
+    B.resize(dim, dim);
+    B.gen_trg(1.1);
+    lll_reduction(B, LLL_DEF_DELTA, LLL_DEF_ETA, LM_WRAPPER);
+    Sieve<Z_NR<ZT>, false> Test_Queue (B);
+    
+}
 
 int main(int argc, char **argv)
 {
 
     cout << "Hello " << endl;
-    //cout << "B[1] = " << B[1] << endl;
+    
+    bool longflag = false; //to be read_in as input
+    int dim[] = {25, 30, 35, 40, 42, 44, 46, 48, 50, 52, 54, 56};
+    
+    if (longflag)
+    {
+        test_run_sieve<long>(dim[0]);
+    }
+    
+    else
+    {
+        test_run_sieve<mpz_t>(dim[0]);
+    }
+    
     //auto start = std::chrono::high_resolution_clock::now();
 //    auto finish = std::chrono::high_resolution_clock::now();
 //    auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(finish-start);
