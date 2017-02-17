f975c2e21103517d20753a63bc9bf1a608265af6
Gottfried Herold <gottfried.herold@rub.de>
2017-02-17 15:48:01 +0100

    Demo for output.

--- fplll/sieve/test_newsieve.cpp
+++ fplll/sieve/test_newsieve.cpp
@@ -23,29 +23,32 @@ void test_run_sieve(int dim)
     lll_reduction(B, LLL_DEF_DELTA, LLL_DEF_ETA, LM_WRAPPER);
     Sieve<Z_NR<ZT>, false> Test_Queue (B);
     //Test_Queue.run_2_sieve(term_cond); //file-name to output the results of tests
-    
+    {
+    std::ofstream ofs("FILENAME");
+    Test_Queue.print_status(-1,ofs);
+    }
 }
 
 int main(int argc, char **argv)
 {
 
     cout << "Hello " << endl;
-    
+
     bool longflag = false; //to be read_in as input
     int dim[] = {25, 30, 35, 40, 42, 44, 46, 48, 50, 52, 54, 56};
     int length = 12;
-    
+
     if (longflag)
     {
         for (int i=0; i<length; i++)
             test_run_sieve<long>(dim[i]);
     }
-    
+
     else
     {
         test_run_sieve<mpz_t>(dim[0]);
     }
-    
+
     //auto start = std::chrono::high_resolution_clock::now();
 //    auto finish = std::chrono::high_resolution_clock::now();
 //    auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(finish-start);
