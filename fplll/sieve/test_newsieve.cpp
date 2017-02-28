d7da4aca0f0fa02efeeb45dafbc82b4fbfe7e736
Elena <elena@dhcp-13-102.lip.ens-lyon.fr>
2017-02-28 18:22:51 +0100

    ofstream append in progress

--- fplll/sieve/test_newsieve.cpp
+++ fplll/sieve/test_newsieve.cpp
@@ -14,7 +14,7 @@ using namespace fplll;
 
 // Z_NR can be either long, double, or mpz_t
 
-template <class ZT> void test_run_sieve(int dim)
+template <class ZT> void test_run_sieve(int dim, std::ofstream &ofs)
 {
     //TerminationConditions< Z_NR<ZT> > term_cond; //sets target-legnth as Mink. bound
    
@@ -28,24 +28,24 @@ template <class ZT> void test_run_sieve(int dim)
     BTest.resize(dim, dim);
     BTest.gen_trg(1.1);
     lll_reduction(BTest, LLL_DEF_DELTA, LLL_DEF_ETA, LM_WRAPPER);
-    Sieve<Z_NR<ZT>, false> Test_Queue2 (BTest);
-    Test_Queue2.run_2_sieve();
+    Sieve<Z_NR<ZT>, false> Test_Queue (BTest);
+    
+    auto start = std::chrono::high_resolution_clock::now();
+    Test_Queue.run_2_sieve();
+    auto finish = std::chrono::high_resolution_clock::now();
+    auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(finish-start);
 
+    Test_Queue.print_status(-1,ofs);
     
     
-    
-    
-    {
-        //std::ofstream ofs("FILENAME");
-        //Test_Queue.print_status(-1,ofs);
-    }
+    cout << " Time taken: " << microseconds.count()/1000000.0 << "sec" << endl;
 }
 
 int main(int argc, char **argv)
 {
     bool longflag = false; //to be read_in as input
-    int dim[] = {25, 30, 35, 40, 42, 44, 46, 48, 50, 52, 54, 56};
-    int length = 12;
+    int dim[] = {60, 62};
+    int length = 2;
 
     if (longflag)
     {
@@ -55,10 +55,11 @@ int main(int argc, char **argv)
 
     else
     {
+        std::ofstream ofs("test_sive.txt");
         ZZ_mat<mpz_t> B;
         B.resize(dim[0], dim[0]);
         B.gen_trg(1.1);
-        test_run_sieve<mpz_t>(dim[0]);
+        test_run_sieve<mpz_t>(dim[0], ofs);
     }
 
     //auto start = std::chrono::high_resolution_clock::now();
