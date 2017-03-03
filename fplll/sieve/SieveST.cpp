f4582fc3ee2fed5a5083bfd098d7f628e0611a4e
Elena <elena@dhcp-13-102.lip.ens-lyon.fr>
2017-03-03 16:32:12 +0100

    not important misc

--- fplll/sieve/SieveST.cpp
+++ fplll/sieve/SieveST.cpp
@@ -25,9 +25,9 @@ void Sieve<ET,false>::run_2_sieve()
     {
         p=main_queue.true_pop();
         SieveIteration2(p);
-        //cout << i <<  " list size" << current_list_size << " Queue: " << main_queue.size() << endl << flush;
+        cout << i <<  " list size" << current_list_size << " Queue: " << main_queue.size() << endl << flush;
         ++i;
-        if (i % 500 == 0) {
+        if (i % 100 == 0) {
             print_status();
             cout << "# of collisions: " << number_of_collisions << endl;
             cout << "norm2 of the so far shortest vector: " << get_best_length2() << endl;
